b99863de9b707433d88b80971fe34d7a10cca200
yoda <98830917+ydt-yash-1738@users.noreply.github.com>
2024-02-29 23:21:32 +0530

    Update UserInfo.jsx

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -1,5 +1,5 @@
 "use client";
-import VideoBackground from "./VideoBackground";
+
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 
@@ -7,75 +7,60 @@ export default function UserInfo() {
   const { data: session } = useSession();
 
   return (
-    <div className="relative">
-      <VideoBackground />
-      <div className="absolute inset-0 flex flex-col justify-center items-center">
-        <div className="shadow-lg p-8 bg-zince-300/10 flex flex-col gap-2 my-6 bg-white rounded-lg opacity-60">
-          <div className="opacity-100">
-            Name: <span className="font-bold">{session?.user?.name}</span>
+    <div className="bg-cover bg-center min-h-screen flex justify-center items-center" style={{backgroundImage: "url('/photos/1.jpg')" }}>
+      <div className="shadow-lg bg-opacity-80 bg-white rounded-lg p-4 md:p-8 text-center">
+        <div className="font-bold text-base md:text-lg mb-4">Name: {session?.user?.name}</div>
+        <div className="font-bold text-base md:text-lg mb-4">Email: {session?.user?.email}</div>
+        <button
+          onClick={() => signOut()}
+          className="bg-cyan-600 text-white font-bold px-4 md:px-6 py-2 mt-4"
+        >
+          Log Out
+        </button>
+        <div className="mt-6">
+          <h1 className="text-lg md:text-xl font-bold">Thank you for using our service!</h1>
+          <div className="font-bold underline mt-4">
+            Let's connect:
           </div>
-          <div className="opacity-100">
-            Email: <span className="font-bold">{session?.user?.email}</span>
-          </div>
-          <button
-            onClick={() => signOut()}
-            className="bg-cyan-600 text-white font-bold px-6 py-2 mt-3 opacity-100"
-          >
-            Log Out
-          </button>
-          <div className="shadow-lg p-8 bg-zince-300/10 flex flex-col gap-2 my-6 rounded-lg opacity-80">
-          <h1 className="text-xl font-extrabold">Thank you so much for using our service!!</h1>
-          
-          <div className="opacity-100 text-bold underline">
-            
-            Let&#39;s connect <span className="font-extrabold"></span>
-          </div>
-          <div className="opacity-100">
+          <div className="mt-2">
             <a
               href="https://www.linkedin.com/in/ydt1738/"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-black font-extrabold hover:underline"
+              className="text-black font-bold hover:underline text-sm md:text-base"
             >
               LinkedIn - Yashdev Tiwari
             </a>
           </div>
-          <div className="opacity-100">
+          <div className="mt-2">
             <a
               href="https://github.com/ydt-yash-1738"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-black font-extrabold hover:underline"
+              className="text-black font-bold hover:underline text-sm md:text-base"
             >
               GitHub
             </a>
           </div>
-          <div className="opacity-100">
+          <div className="mt-2">
             <a
               href="https://ydt-yash-1738.github.io/Portfolio/"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-black font-extrabold hover:underline"
+              className="text-black font-bold hover:underline text-sm md:text-base"
             >
               Portfolio
             </a>
           </div>
-            <div className="opacity-100">
-              <a
-                href="https://mail.google.com/mail/?view=cm&to=yashdt50@gmail.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-black font-extrabold hover:underline"
-              >
-                Email
-              </a>
-            </div>
-
-          
+          <div className="mt-2">
+            <a
+              href="mailto:yashdt50@gmail.com"
+              className="text-black font-bold hover:underline text-sm md:text-base"
+            >
+              Email
+            </a>
+          </div>
         </div>
-        </div>
-        
-
       </div>
     </div>
   );
