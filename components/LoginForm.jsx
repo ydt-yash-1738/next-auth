f3be403d2a3962620170f99eca0b45cfc128eba6
yoda <98830917+ydt-yash-1738@users.noreply.github.com>
2024-02-29 23:20:35 +0530

    Update LoginForm.jsx

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -34,32 +34,34 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="grid place-items-center h-screen bg-cyan-800">
-      <div className="shadow-2xl p-5 rounded-lg border-t-4 border-green-500 bg-white">
-        <h1 className="text-xl font-bold my-4">Login</h1>
+    <div className="bg-cover bg-center min-h-screen flex justify-center items-center brightness-70" style={{backgroundImage: "url('/photos/3.jpg')" }}>
+      <div className="max-w-md w-full shadow-lg p-5 rounded-lg border-t-4 border-green-500 bg-white opacity-80">
+        <h1 className="text-xl md:text-2xl font-bold my-4">Login</h1>
 
         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <input
             onChange={(e) => setEmail(e.target.value)}
             type="text"
             placeholder="Email"
+            className="px-3 py-2 rounded-md border text-sm md:text-base"
           />
           <input
             onChange={(e) => setPassword(e.target.value)}
             type="password"
             placeholder="Password"
+            className="px-3 py-2 rounded-md border text-sm md:text-base"
           />
-          <button className="bg-cyan-600 text-white font-bold cursor-pointer px-6 py-2">
+          <button className="bg-cyan-600 text-white font-bold cursor-pointer px-6 py-2 text-sm md:text-base">
             Login
           </button>
           {error && (
-            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+            <div className="bg-red-500 text-white w-fit text-xs md:text-sm py-1 px-3 rounded-md mt-2">
               {error}
             </div>
           )}
 
-          <Link className="text-sm mt-3 text-right" href={"/register"}>
-            Don&#39;t have an account? <span className="underline">Register</span>
+          <Link className="text-xs md:text-sm mt-3 text-right" href={"/register"}>
+            Don't have an account? <span className="underline">Register</span>
           </Link>
         </form>
       </div>
